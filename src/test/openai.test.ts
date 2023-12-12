90615b163b7b3e7af5ad8edc7ecceca01266049f
lwd3737 <36585925+lwd3737@users.noreply.github.com>
2023-12-13 03:11:03 +0900

    setup dev environment

---
+++ src/test/openai.test.ts
@@ -0,0 +1,16 @@
+import { describe } from "node:test";
+import OpenAI from "openai";
+
+describe("Assistant API", () => {
+	const openai = new OpenAI();
+
+	test("should chat completion be created", async () => {
+		const completion = await openai.chat.completions.create({
+			messages: [{ role: "system", content: "You are a helpful assistant." }],
+			model: "gpt-3.5-turbo",
+		});
+
+		console.log("result", completion);
+		expect(completion.choices[0]).toBeTruthy();
+	});
+});
