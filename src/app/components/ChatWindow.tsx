070fbea89a3fb99af2c2113d1b9969e0ab3a84a6
lwd3737 <36585925+lwd3737@users.noreply.github.com>
2024-01-15 02:24:18 +0900

    modify: MessageForm ui layout and logic

--- src/app/components/ChatWindow.tsx
+++ src/app/components/ChatWindow.tsx
@@ -1,13 +1,20 @@
 "use client";
 
+import { useState } from "react";
 import ChatHistoryBox from "./ChatHistoryBox";
 import MessageForm from "./MessageForm";
 
 export default function ChatWindow() {
+	const [chatHistory, setChatHistory] = useState<string[]>([]);
+
+	const handleSendMessage = (message: string) => {
+		setChatHistory((prev) => [...prev, message]);
+	};
+
 	return (
 		<div className="flex flex-col h-full">
 			<ChatHistoryBox />
-			<MessageForm />
+			<MessageForm onSendMessage={handleSendMessage} />
 		</div>
 	);
 }
--- src/app/components/MessageForm.tsx
+++ src/app/components/MessageForm.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import {
+	FormEventHandler,
+	KeyboardEventHandler,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
+import ArrowUpIcon from "./ArrowUpIcon";
 
-type MessageFormProps = {};
+type MessageFormProps = {
+	onSendMessage: (message: string) => void;
+};
 
 export default function MessageForm(props: MessageFormProps) {
 	const textareaRef = useRef<HTMLTextAreaElement>(null);
 
 	useEffect(function onAutoResize() {
 		const el = textareaRef.current;
-		if (!el) return;
+		if (!el) {
+			console.error(ERRORS.textareaElementNotExist);
+			return;
+		}
 
 		const autoResize = () => {
 			if (el.scrollHeight >= MAX_HEIGHT) {
@@ -24,16 +36,80 @@ export default function MessageForm(props: MessageFormProps) {
 		return () => el.removeEventListener("input", autoResize);
 	}, []);
 
+	const [activated, setActivated] = useState<boolean>(false);
+	useEffect(function toggleSubmitActivatedOnInput() {
+		const el = textareaRef.current;
+		if (!el) {
+			console.error(ERRORS.textareaElementNotExist);
+			return;
+		}
+
+		const toggleSubmitActivated = () => {
+			const textLength = el.value.length;
+			console.log(textLength);
+
+			if (textLength > 0) setActivated(true);
+			else setActivated(false);
+		};
+
+		el.addEventListener("input", toggleSubmitActivated);
+		return () => el.removeEventListener("input", toggleSubmitActivated);
+	}, []);
+
+	const handleSubmit: FormEventHandler = (e) => {
+		e.preventDefault();
+
+		const el = e.target as HTMLTextAreaElement;
+		const message = el.value;
+
+		props.onSendMessage(message);
+	};
+
+	const handleKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
+		if (e.key === "Enter" && !e.shiftKey) {
+			e.preventDefault();
+
+			if (activated) {
+				handleSubmit(e);
+			}
+		}
+	};
+
 	return (
-		<form className="px-5 py-3">
-			<textarea
-				className={`w-full h-[${DEFAULT_HEIGHT}px] min-h-[${DEFAULT_HEIGHT}px] px-[16px] py-[14px] leading-[20px] text-[15px] bg-gray-200 resize-none rounded-3xl`}
-				ref={textareaRef}
-				rows={1}
-			></textarea>
+		<form className="px-5 py-3" onSubmit={handleSubmit}>
+			<div className="px-[16px] py-[14px] bg-gray-100 rounded-3xl">
+				<textarea
+					className="w-full leading-[20px] text-[15px] bg-inherit resize-none outline-none"
+					ref={textareaRef}
+					rows={1}
+					onKeyDown={handleKeyDown}
+				></textarea>
+				<div className="flex justify-end">
+					<button
+						className={`p-1 border-2 border-${colorBasedOnActivated(
+							activated,
+						)} border-solid rounded-md`}
+						type="submit"
+						disabled={!activated}
+					>
+						<ArrowUpIcon
+							className={`w-[13px] h-[13px] fill-${colorBasedOnActivated(
+								activated,
+							)}`}
+						/>
+					</button>
+				</div>
+			</div>
 		</form>
 	);
 }
 
 const MAX_HEIGHT = 150;
-const DEFAULT_HEIGHT = 48;
+
+const ERRORS = {
+	textareaElementNotExist: "textarea element not exist",
+};
+
+const colorBasedOnActivated = (activated: boolean) => {
+	return activated ? "[#4058FF]" : "[gray]";
+};
