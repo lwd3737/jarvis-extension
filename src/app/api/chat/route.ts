b7ddf07012acb9c6c6f2fdb07c070476f415b2ec
lwd3737 <36585925+lwd3737@users.noreply.github.com>
2024-02-17 19:48:49 +0900

    fix: route logic

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,19 +1,12 @@
 import { getChatCompletionService } from "@/src/services";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { CompletionCreateRequestBody } from "./fetch";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
 export async function POST(req: NextRequest) {
 	const service = getChatCompletionService();
 	const body = (await req.json()) as CompletionCreateRequestBody;
-	//
-	const ai = service.getOpenai();
-	const res = await ai.chat.completions.create({
-		messages: body.messages,
-		model: service.getModel(),
-		stream: true,
-	});
+	const res = await service.createCompletion(body.messages);
 	const stream = OpenAIStream(res);
-
 	return new StreamingTextResponse(stream);
 }
